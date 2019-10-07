869cd1bbe7109ccd6782feeed4325e7a389af6dd
Vladas Maier <vladas.mr@googlemail.com>
2019-10-07 20:25:37 +0200

    Added CreateUserActivity

---
+++ app/src/main/java/org/vmaier/smack/CreateUserActivity.kt
@@ -0,0 +1,26 @@
+package org.vmaier.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateBackgroundColorButtonClicked(view: View) {
+
+    }
+
+    fun createUserButtonClicked(view: View) {
+
+    }
+}
--- app/src/main/java/org/vmaier/smack/LoginActivity.kt
+++ app/src/main/java/org/vmaier/smack/LoginActivity.kt
@@ -1,8 +1,10 @@
 package org.vmaier.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -17,5 +19,7 @@ class LoginActivity : AppCompatActivity() {
 
     fun loginCreateUserButtonClicked(view: View) {
 
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 }
